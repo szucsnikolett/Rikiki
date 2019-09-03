adb813fe86a3a73512d113fa029a9f0a62fbe5e3
Agnes Kadlot <agnes.kadlot@gmail.com>
2019-09-03 14:11:36 +0200

    Modify Bet Round

--- static/js/main.js
+++ static/js/main.js
@@ -11,15 +11,28 @@ function gameRules(){
     })
 }
 
-function getBets(){
-    let players = ["player1", "player2", "player3", "player4"];
-    let bets = {};
-    for (let player of players){
-        let bet = prompt(`Make your bets now! ${player}`);
-        bets[player] = bet;
-    }
-    return bets
+
+
+function getBets(player){
+    let bet = prompt(`Make your bets now! ${player}`;
+    return bet
 }
 
-getBets();
+
+function checkBets(bets, actualResult, scores){
+    for (let key in bets){
+        if (bets.hasOwnProperty(key)){
+            if (bets[key] === actualResult[key]){
+                scores[key] += 10;
+            }
+        }
+    }
+}
+
+let bets = {} ;
+let bet = getBets(player);
+bets[player] = bet;
+checkBets(bets, actualResult, scores);
+
 gameRules();
+
